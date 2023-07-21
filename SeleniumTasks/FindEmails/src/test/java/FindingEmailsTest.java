f19e61c2a8b20b90288d5eeb14acd77a87d64807
smbatshahbazyan <smbatshahbazyan@mail.com>
2023-07-21 19:45:03 +0400

    Add some changes

--- SeleniumTasks/FindEmails/src/test/java/FindingEmailsTest.java
+++ SeleniumTasks/FindEmails/src/test/java/FindingEmailsTest.java
@@ -8,10 +8,10 @@ public class FindingEmailsTest extends BaseTest {
     @Test
     public void findingEmails() {
         obj.get("https://practice.expandtesting.com/tables");
-        List<WebElement> emails = obj.findElements(By.xpath("//*[@id='table2']//td[contains(@class, 'email')]"));
+        List<WebElement> emails = obj.findElements(By.xpath("//table[@id='table2']//td[contains(@class, 'email')]"));
         for (WebElement email : emails) {
             System.out.println(email.getText());
         }
-        obj.findElement(By.xpath("//*[@id='table2']//tbody/tr/td[text() = 'Smith']//..//following-sibling:: a[contains(@class, 'btn btn-danger')]")).click();
+        obj.findElement(By.xpath("//table[@id='table2']//tbody/tr/td[text() = 'Smith']//..//following-sibling:: a[contains(@class, 'btn btn-danger')]")).click();
     }
 }
